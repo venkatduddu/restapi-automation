ef916f0403d9f16fd465666b0b7686bd4111bd2a
Venkat Duddu <vduddu@chegg.com>
2018-11-03 18:22:03 -0700

    Implement PUT employee and add support for search employee by name

--- src/main/java/com/automation/restapi/restapiautomation/controller/EmployeeController.java
+++ src/main/java/com/automation/restapi/restapiautomation/controller/EmployeeController.java
@@ -7,12 +7,15 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,8 +37,12 @@ public class EmployeeController {
     return ResponseEntity.notFound().build();
   }
 
-  @GetMapping()
-  public ResponseEntity<List<Employee>> getAllEmployee() {
+  @GetMapping
+  public ResponseEntity<List<Employee>> getAllEmployee(@RequestParam("name") String name) {
+
+    if(StringUtils.hasText(name)){
+      return ResponseEntity.ok(employeeRepository.findByName(name));
+    }
 
     List<Employee> employees = employeeRepository.findAll();
 
@@ -46,6 +53,15 @@ public class EmployeeController {
   public ResponseEntity<Employee> saveEmployeeB(@RequestBody Employee employee) {
 
     Employee savedEmplyee = employeeRepository.saveAndFlush(employee);
+
+    return ResponseEntity.ok(savedEmplyee);
+  }
+
+  @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "{id}")
+  public ResponseEntity<Employee> saveEmployeeB(@RequestBody Employee employee, @PathVariable("id") Long id) {
+    employee.setId(id);
+
+    Employee savedEmplyee = employeeRepository.save(employee);
 
     return ResponseEntity.ok(savedEmplyee);
   }
--- src/main/java/com/automation/restapi/restapiautomation/repository/EmployeeRepository.java
+++ src/main/java/com/automation/restapi/restapiautomation/repository/EmployeeRepository.java
@@ -1,8 +1,13 @@
 package com.automation.restapi.restapiautomation.repository;
 
 import com.automation.restapi.restapiautomation.model.Employee;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+  @Query("select e from Employee e where e.name like %?1%")
+  List<Employee> findByName(String name);
 
 }
